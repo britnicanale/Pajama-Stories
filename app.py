baaf0f964a5afb673b1976272a6bd045f0e827dd
jchen59 <jchen59@stuy.edu>
2018-10-17 09:16:57 -0400

    updated devlog

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__) # instantiates an instance of Flask
 
 @app.route("/") #Linking a function to a route
 def home():
-    return "Hello, World!"
+    return render_template("story.html")
 
 if __name__ == "__main__":
     app.debug = True
