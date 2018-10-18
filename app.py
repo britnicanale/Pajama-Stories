4dd42ed0ac51405eceef06962883e97bffef95bf
Jason Lin <jasonlin43212@gmail.com>
2018-10-18 10:39:40 -0400

    removed unused code

--- app.py
+++ app.py
@@ -7,22 +7,6 @@ from flask import Flask, render_template, request
 import sqlite3
 app = Flask(__name__) # instantiates an instance of Flask
 
-DB_FILE="data/bigData.db"
-
-db = sqlite3.connect(DB_FILE)
-c = db.cursor()
-#create users
-command = "CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT UNIQUE,password_hash TEXT)"
-c.execute(command)
-#creat contributions
-command = "CREATE TABLE contributions(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER,story_id INTEGER,body TEXT)"
-c.execute(command)
-#create stories
-command = "CREATE TABLE stories(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT)"
-c.execute(command)
-db.commit()
-db.close()
-
 @app.route("/") #Linking a function to a route
 def home():
     return render_template("story_contributed.html", additions = [("the duck goes woof","dennis"),("the dog goes meow", "jason"), ("the cat goes quack","kenny")])
