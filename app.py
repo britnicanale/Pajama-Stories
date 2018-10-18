12f222d0b8fd8dc433da7c5765abb8a053a1f64c
Jason Lin <jasonlin43212@gmail.com>
2018-10-18 10:38:33 -0400

    moved peter's data creation file

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-#Jason Lin
+#PJ Cubed -- Jiayang Chen, Jabir Chowdhury, Peter Cwalina, Jason Lin
 #SoftDev1 pd07
-#KNN -- TITLE
-#201y-mm-dd
+#P00 -- Da Art of Storytellin'
+#2018-10-17
 
 from flask import Flask, render_template, request
+import sqlite3
 app = Flask(__name__) # instantiates an instance of Flask
+
+DB_FILE="data/bigData.db"
+
+db = sqlite3.connect(DB_FILE)
+c = db.cursor()
+#create users
+command = "CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT UNIQUE,password_hash TEXT)"
+c.execute(command)
+#creat contributions
+command = "CREATE TABLE contributions(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER,story_id INTEGER,body TEXT)"
+c.execute(command)
+#create stories
+command = "CREATE TABLE stories(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT)"
+c.execute(command)
+db.commit()
+db.close()
 
 @app.route("/") #Linking a function to a route
 def home():
--- makefile
+++ makefile
@@ -1,3 +1,6 @@
 all: app.py
 	rm data/bigData.db
 	python app.py
+
+db:
+	rm data/bigData.db
