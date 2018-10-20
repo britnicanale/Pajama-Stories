fc8e2b17189154582d2230f0362be19c71306a65
jiayang <jchen59@stuy.edu>
2018-10-20 18:32:00 -0400

    made code cleaner and added comments

--- app.py
+++ app.py
@@ -16,23 +16,15 @@ user_data = db.get_all_user_data()
 def is_logged_in():
     return "id" in session
 
-@app.route("/") #Linking a function to a route
+@app.route("/") #Root
 def login():
     if(is_logged_in()):
-        #index to 0th because that is the id
         contributed_data = db.get_user_contribution(session["id"]);
-        list_contr_story_id = []
-        for tuple in contributed_data:
-            list_contr_story_id.append(tuple[0])
-        allStories = db.get_all_stories()
-        title_list = []
-        non_title_list = []
-        for story_id in list_contr_story_id:
-            title_list.append(db.get_title(story_id))
-        for story_id in allStories:
-            if story_id[0] not in list_contr_story_id:
-                non_title_list.append(db.get_title(story_id[0]))
-        return render_template("homepage.html", titles = title_list, non_titles = non_title_list)
+        list_contr_story_id = [x[0] for x in contributed_data] # x[0] is the story id
+        allStories = db.get_all_stories() # List of all the stories (id)
+        contr_title_list = [db.get_title(story_id) for story_id in list_contr_story_id] # List of all contributed-to story titles
+        uncontr_title_list = [db.get_title(story_id) for story_id in allStories if story_id not in list_contr_story_id] # List of all uncontributed-to story titles
+        return render_template("homepage.html", titles = contr_title_list, non_titles = uncontr_title_list)
     else:
         return render_template("login.html")
 
@@ -40,6 +32,7 @@ def login():
 def authenticate():
     username_input = request.form.get("username")
     password_input = request.form.get("password")
+    #Checks if user/pass is valid if not flash reasons why
     if username_input in user_data:
         if user_data[username_input]  == password_input:
             id = db.get_user_id(username_input)
@@ -59,11 +52,11 @@ def logout():
 def view_story(story_id):
     if not is_logged_in():
         return redirect(url_for("login"))
-
+    #If story is not in the list of all stories
     if story_id not in [i[0] for i in db.get_all_stories()]:
         return render_template("story_unfound.html")
-    additions = db.get_story_body(story_id)
-    users = [user[0] for user in additions]
+    additions = db.get_story_body(story_id) #List of all user additions to specific story
+    users = [user[0] for user in additions] #List of all users who made the additions
     if session["id"] in users:
         return render_template("story_contributed.html", story_id = story_id, story_body = additions)
     return render_template("story_uncontributed.html", story_id = story_id, story_body = additions)
@@ -72,8 +65,8 @@ def view_story(story_id):
 def add_contribution(story_id):
     if not is_logged_in():
         return redirect(url_for("login"))
-        
-    db.add_contribution(session["id"], story_id, request.form.get("addition"))
+
+    db.add_contribution(session["id"], story_id, request.form.get("addition")) #Adds into specific story table: (usr_id, story_id, body of text)
     flash("Successfully added to the story!")
     return redirect(url_for("view_story", story_id = story_id))
 
