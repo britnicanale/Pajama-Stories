fea66d7c1139fe3aecb6b8ca4d86b39657966c2e
jchen59 <jchen59@stuy.edu>
2018-10-22 09:16:03 -0400

    cleanup crew and fixed minor bugs

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def is_logged_in():
     return "id" in session
 
 @app.route("/") #Root
-def login():
+def home():
     if(is_logged_in()):
         contributed_data = db.get_user_contribution(session["id"]);
         list_contr_story_id = [x[0] for x in contributed_data] # x[0] is the story id
@@ -27,7 +27,11 @@ def login():
         uncontr_title_list = [(db.get_title(str(story_id[0])),story_id[0]) for story_id in allStories if story_id[0] not in list_contr_story_id] # List of all uncontributed-to story titles
         return render_template("homepage.html", titles = contr_title_list, non_titles = uncontr_title_list, user=db.get_username(session["id"]))
     else:
-        return render_template("login.html")
+        return redirect(url_for("login"))
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 @app.route("/auth" , methods = ["POST"])
 def authenticate():
@@ -44,12 +48,12 @@ def authenticate():
             flash("Invalid password, try again!")
     else:
         flash("Invalid username, try again!")
-    return redirect(url_for('login'))
+    return redirect(url_for('home'))
 
 @app.route("/logout", methods=["GET"])
 def logout():
     session.pop("id")
-    return redirect(url_for("login"))
+    return redirect(url_for("home"))
 
 @app.route("/register")
 def register():
@@ -66,7 +70,7 @@ def reg_auth():
         return redirect(url_for("register"))
     #checks if the user typed the same passward twice
     elif len(username_input) < 4:
-        flash("Username has to be atleast 4 characters!")
+        flash("Username has to be at least 4 characters!")
         return redirect(url_for("register"))
     elif password_input != password_input2:
         flash("Input Same Password in Both Fields!")
@@ -76,13 +80,13 @@ def reg_auth():
         #version of his password to database
         db.add_user(username_input, md5_crypt.encrypt(password_input))
         flash("Successfully Registered, Now Sign In!")
-        return redirect(url_for('login'))
+        return redirect(url_for('home'))
 
 
 @app.route("/story/<int:story_id>")
 def view_story(story_id):
     if not is_logged_in():
-        return redirect(url_for("login"))
+        return redirect(url_for("home"))
     #If story is not in the list of all stories
     if story_id not in [i[0] for i in db.get_all_stories()]:
         return render_template("story_unfound.html")
@@ -96,7 +100,7 @@ def view_story(story_id):
 @app.route("/story/<int:story_id>/add", methods = ["POST"])
 def add_contribution(story_id):
     if not is_logged_in():
-        return redirect(url_for("login"))
+        return redirect(url_for("home"))
 
     db.add_contribution(session["id"], story_id, request.form.get("addition")) #Adds into specific story table: (usr_id, story_id, body of text)
     flash("Successfully added to the story!")
@@ -104,10 +108,16 @@ def add_contribution(story_id):
 
 @app.route("/create")
 def create_story():
+    if not is_logged_in():
+        return redirect(url_for("home"))
+
     return render_template("create_story.html", user=db.get_username(session["id"]))
 
-@app.route("/creating", methods = ["POST"])
+@app.route("/creating", methods = ["POST","GET"])
 def creating_story():
+    if not is_logged_in():
+        return redirect(url_for("home"))
+
     db.add_story(request.form.get("title"))
     new_story_id = db.get_all_stories()[-1][0]
     db.add_contribution(session["id"],new_story_id,request.form.get("body"))
